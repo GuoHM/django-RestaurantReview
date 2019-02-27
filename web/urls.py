1b42c08fc4e29a56415aa0e712e5aaa846a3d8a9
Guo Haoming <fsguohaoming@hotmail.com>
2019-02-27 21:05:48 +0800

    finish login logout

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.list, name='index'),
-    path('list',views.list)
+    path('list',views.list),
+    path('login',views.login_page),
+    path('loginAction',views.login_action),
+    path('logout',views.logout),
+    path('/detail/<name>', views.detail, name='detail'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import *
 # Create your views here.
 
 
 def list(request):
-    context = {
-        'text': 'hello'
-    }
-    return render(request, 'list.html', context)
+    restaurants = Restaurant.objects.all();
+    return render(request, 'list.html', {'restaurantlist':restaurants})
+
+
+def login_page(request):
+    return render(request, 'login.html')
+
+
+def login_action(request):
+    user = User.objects.get(user_name=request.POST['username'])
+    if user.password == request.POST['password']:
+        request.session['userid'] = user.user_id
+        return render(request, 'list.html',{'text':'success'})
+    else:
+        return render(request, 'login.html', {'message': 'Invalid login attempt!'})
+
+
+def logout(request):
+    del request.session['userid']
+    return render(request, 'login.html')
+
+
+def detail(request, name):
+    restaurants = Restaurant.objects.get(name=name);
+    return render(request, 'detail.html', {'name': name})
